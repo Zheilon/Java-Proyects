cc58f545800df4a1db9d9dff58728b6ec56c86c8
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-24 16:09:21 -0500

    Variables For Nodes Created!

--- CalculatorTwo/src/main/java/app/company/play/calculatortwo/ControllerFirst.java
+++ CalculatorTwo/src/main/java/app/company/play/calculatortwo/ControllerFirst.java
@@ -1,14 +1,90 @@
 package app.company.play.calculatortwo;
-
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
-public class ControllerFirst {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class ControllerFirst implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
+
+    @FXML
+    private Label textUp;
+
+    @FXML
+    private Label textDown;
+
+    @FXML
+    private Button division;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    private Button per;
+
+    @FXML
+    private Button rest;
+
+    @FXML
+    private Button sum;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button deleteAll;
+
+    @FXML
+    void deleteAllButton(MouseEvent event)
+    {
+        textDown.setText("");
+    }
+
+    @FXML
+    private void zeroButton(MouseEvent event)
+    {
+        textDown.setText(textDown.getText() + "0");
+    }
+
+    @FXML
+    private void oneButton(MouseEvent event)
+    {
+        textDown.setText(textDown.getText() + "1");
+    }
+
 }
