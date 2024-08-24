0e594e016e9ec6811a9381e4c1722c0499e757c1
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-24 17:10:57 -0500

    Add new Functions: Cursor Show / set Numbers

--- CalculatorTwo/src/main/java/app/company/play/calculatortwo/ControllerFirst.java
+++ CalculatorTwo/src/main/java/app/company/play/calculatortwo/ControllerFirst.java
@@ -6,12 +6,22 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ControllerFirst implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        List<Button> listButton = new ArrayList<>(List.of(zero,
+                one, two, three, four, five, six, seven, eight,
+                nine, deleteAll, division, per, rest, sum, equal));
+
+        for (Button z : listButton) {
+            z.setOnMouseEntered(InteractController.cursorHandShow(z).getOnMouseEntered());
+        }
 
     }
 
@@ -72,18 +82,29 @@ public class ControllerFirst implements Initializable {
     @FXML
     void deleteAllButton(MouseEvent event)
     {
+        textUp.setText("");
         textDown.setText("");
+        textUp.setStyle("-fx-background-color: rgba(250, 219, 216, 1);");
     }
 
     @FXML
-    private void zeroButton(MouseEvent event)
-    {
+    void zeroButton(MouseEvent event) {
+        if (!textUp.getText().isEmpty()) {
+            textUp.setStyle("-fx-background-color: rgba(230, 176, 170, 0.5);");
+        }
+
+        textUp.setText(textUp.getText() + "0");
         textDown.setText(textDown.getText() + "0");
     }
 
     @FXML
     private void oneButton(MouseEvent event)
     {
+        if (!textUp.getText().isEmpty()) {
+            textUp.setStyle("-fx-background-color: rgba(230, 176, 170, 0.5);");
+        }
+
+        textUp.setText(textUp.getText() + "1");
         textDown.setText(textDown.getText() + "1");
     }
 
