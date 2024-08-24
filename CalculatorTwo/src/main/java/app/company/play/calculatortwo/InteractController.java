81f110a2801230d4b88be3c4b6981a6b6f375636
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-24 17:54:18 -0500

    New File

---
+++ CalculatorTwo/src/main/java/app/company/play/calculatortwo/InteractController.java
@@ -0,0 +1,11 @@
+package app.company.play.calculatortwo;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class InteractController {
+
+    public static Button cursorHandShow (Button button)
+    { button.setCursor(Cursor.HAND); return button; }
+
+}
