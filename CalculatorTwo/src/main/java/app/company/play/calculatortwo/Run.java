36165756a354b94e60d857d428a452c53df953b7
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-24 15:25:43 -0500

    Update Class Run!

--- CalculatorTwo/src/main/java/app/company/play/calculatortwo/Run.java
+++ CalculatorTwo/src/main/java/app/company/play/calculatortwo/Run.java
@@ -2,7 +2,9 @@ package app.company.play.calculatortwo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -10,9 +12,14 @@ public class Run extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Run.class.getResource("viewFirst.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
